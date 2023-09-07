57ca788e88b5bd25ffaec90e138aa699250d91c9
Christensen-John <121368802+Christensen-John@users.noreply.github.com>
2023-09-07 10:55:50 +0100

    fix column number

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -3,15 +3,29 @@ const sketchContainer = document.querySelector("div.sketch");
 
 /* !! Functions */
 function createSketchWindow(parentElem, numRows, numCol) {
-  createRow(parentElem, numRows);
-  createCol(parentElem, numCol);
+  createRows(parentElem, numRows);
+  const sketchRows = document.querySelectorAll("div.sketch>div");
+  createCols(sketchRows, numCol);
 }
 
-function createRow(parentElem, numRows) {
+function createRows(parentElem, numRows) {
   for (let row = 0; row < numRows; row++) {
     let div = document.createElement("div");
     div.setAttribute("data-row", `${row}`);
+    parentElem.appendChild(div);
   }
 }
 
+//parentElems is a nodelist of multiple rows
+function createCols(parentElems, numCol) {
+  parentElems.forEach((row) => {
+    for (let col = 0; col < numCol; col++) {
+      let div = document.createElement("div");
+      div.setAttribute("data-column", `${col}`);
+      row.appendChild(div);
+    }
+  });
+}
+
 /* !! Execution */
+createSketchWindow(sketchContainer, 16, 16);
